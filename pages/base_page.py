ee4e5927d5f8bf561feee9602e38c9318aedaca3
IlyDja <mak13666tula@gmail.com>
2024-03-25 12:59:44 +0300

    an implicit transition between pages initialized in the body of the testÑ

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,7 @@
-#from selenium.common.exceptions import имя_исключения
+# from selenium.common.exceptions import имя_исключения
 
 
-class BasePage():
+class BasePage:
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
@@ -13,6 +13,6 @@ class BasePage():
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
-        except: # (имя исключения):
+        except:  # (имя исключения):
             return False
         return True
